5a89013251a1e6f4225a236d33520b8d2f42a1c8
unknown <rekha.kumari1928@gmail.com>
2025-05-21 12:18:45 +0530

    populate members in teams

--- models/team.model.js
+++ models/team.model.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String },
-  members: [{ type: String, required: true }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
 });
 module.exports = mongoose.model("Team", teamSchema);
